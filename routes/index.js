c9b97d4d28ee54ccd2e6fe8e74dce63e92502ba7
Ashish <rathorashish79@gmail.com>
2022-05-20 19:21:39 +0530

    Code structure updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const axios = require('axios')
 var bodyParser = require('body-parser')
 const app = express();
 const router = express.Router();
-const User = require('../controllers/user');
 const responseMessage = require('../utils/responseMessage');
 
 // support parsing of application/json type post data
@@ -12,28 +10,18 @@ router.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
-    var user = "Ashish";
-    res.render('home', {user:user});
-})
-
-router.get('/about', (req, res) => {
-    var user = "Ashish";
-    res.render('about', {user:user});
-})
-
 router.get('/search', (req, res) => {
   res.render('search_posts');
 })
 // -------------Post request for insert user--------------------
 
 router.post('/getBlogs',async (req,res)=>{
-    console.log("Get blog try se phle")
     try {
         console.log("Data from postman",req.body);
         res.send({
-          "status":200,
-          "message":"Success"
+          status:200,
+          message:"Success",
+          data:req.body
         })
         // return responseMessage(null, data)
       } catch (err) {
